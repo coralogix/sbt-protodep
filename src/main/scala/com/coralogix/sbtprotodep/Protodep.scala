f62bcdb620818555a9b7ccb3d6f64f68fd9aef6e
Zsolt Szilagyi <zsolt.s@coralogix.com>
2022-01-24 15:13:37 +0100

    Allow different project name from path (#25)
    * Allow different project name from path
    
    This allows e.g. to generate in the root project. The project name will not accept `.`.
    
    * Switch parameters

--- src/main/scala/com/coralogix/sbtprotodep/Protodep.scala
+++ src/main/scala/com/coralogix/sbtprotodep/Protodep.scala
@@ -14,8 +14,8 @@ object Protodep extends AutoPlugin {
 
   import autoImport._
 
-  def generateProject(name: String) =
-    Project(name, file(name))
+  def generateProject(name: String, path: Option[String] = None) =
+    Project(name, file(path.getOrElse(name)))
       .enablePlugins(GrpcDependencies)
       .settings(
         protodepVersion := "0.1.2-1-ge811cd8",
